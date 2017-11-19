3951d3272a8a9c487bc9ad1d26981c782dc4b954
daniparraguez <danielaparraguezvarela@gmail.com>
2017-11-19 18:07:34 -0300

    Actualizando cambios

--- js/app.js
+++ js/app.js
@@ -2,5 +2,19 @@
  * Funcionalidad de tu producto
  */
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+// Puedes hacer uso de la base de datos a través
+// de la variable `data`
+//console.log(data);
+
+(var alumnasScl2016_2 = function(){
+   var container = document.getElementById('scl2016-2');
+   var students = data.SCL["2016-2"].students;
+
+       var total = students.length;
+       var div = document.createElement('div');
+         var nodoParrafo = document.createTextNode(total);
+             div.appendChild(nodoParrafo);
+             container.appendChild(div);
+
+}());
+
