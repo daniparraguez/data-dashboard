b791370f7dee5ee9823eb0878042be3c8239d3a1
saahub <carolina-saavedra1@hotmail.com>
2017-11-19 13:19:58 -0300

    nuevos cambios

--- js/app.js
+++ js/app.js
@@ -3,4 +3,15 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+//console.log(data);
+(function(){
+   var container = document.getElementById('container');
+   var students = data.SCL["2016-2"].students;
+
+       var total = students.length;
+       var parrafo = document.createElement('p');
+         var nodoParrafo = document.createTextNode(total);
+             parrafo.appendChild(nodoParrafo);
+             container.appendChild(parrafo);
+
+}());
