f9d7608d85be60a076a67e6e4f006b6b6fae6ba2
daniparraguez <danielaparraguezvarela@gmail.com>
2017-11-20 11:54:26 -0300

    Actualizando archivo JS

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ var SclStudentsGen20172=data.SCL["2017-2"].students;
 var SclStudentsGen20171=data.SCL["2017-1"].students;
 
 (function (){
-   var container = document.getElementById('sclgen2016');
+   var container = document.getElementById('sclgen20162');
        var total = SclStudentsGen20162.length;
        var div = document.createElement('div');
          var nodoParrafo = document.createTextNode(total);
@@ -21,6 +21,26 @@ var SclStudentsGen20171=data.SCL["2017-1"].students;
              container.appendChild(div);
 
 }());
+
+(function (){
+   var container = document.getElementById('sclgen20172');
+       var total = SclStudentsGen20172.length;
+       var div = document.createElement('div');
+         var nodoParrafo = document.createTextNode(total);
+             div.appendChild(nodoParrafo);
+             container.appendChild(div);
+
+}());
+
+(function (){
+   var container = document.getElementById('sclgen20171');
+       var total = SclStudentsGen20171.length;
+       var div = document.createElement('div');
+         var nodoParrafo = document.createTextNode(total);
+             div.appendChild(nodoParrafo);
+             container.appendChild(div);
+
+}());
 //AREQUIPA - AQP
 //Por generación
 var AqpStudentsGen20162=data.AQP["2016-2"].students;
@@ -36,11 +56,6 @@ var AqpStudentsGen20171=data.AQP["2017-1"].students;
 
 }());
 
-//AREQUIPA - AQP
-//Por generación
-var AqpStudentsGen20162=data.AQP["2016-2"].students;
-var AqpStudentsGen20171=data.AQP["2017-1"].students;
-
 (function (){
    var container = document.getElementById('aqp2016');
        var total = AqpStudentsGen20162.length;
@@ -51,14 +66,30 @@ var AqpStudentsGen20171=data.AQP["2017-1"].students;
 
 }());
 
-//AREQUIPA - AQP
+//LIMA-LIM
 //Por generación
-var AqpStudentsGen20162=data.AQP["2016-2"].students;
-var AqpStudentsGen20171=data.AQP["2017-1"].students;
+var LimStudentsGen20162=data.LIM["2016-2"].students;
+var LimStudentsGen20171=data.LIM["2017-1"].students;
+var LimStudentsGen20172=data.LIM["2017-2"].students;
 
 (function (){
-   var container = document.getElementById('aqp2016');
-       var total = AqpStudentsGen20162.length;
+   var container = document.getElementById('lim2016');
+       var total = LimStudentsGen20162.length;
+       var div = document.createElement('div');
+         var nodoParrafo = document.createTextNode(total);
+             div.appendChild(nodoParrafo);
+             container.appendChild(div);
+
+}());
+
+//MÉXICO - CDMX
+//Por generación
+var CdmxStudentsGen20172=data.CDMX["2017-2"].students;
+var CdmxStudentsGen20171=data.CDMX["2017-1"].students;
+
+(function (){
+   var container = document.getElementById('cdmx2016');
+       var total = CdmxStudentsGen20162.length;
        var div = document.createElement('div');
          var nodoParrafo = document.createTextNode(total);
              div.appendChild(nodoParrafo);
